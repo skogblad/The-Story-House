51259a9de21fb46071a9eee78d16550e87c83460
SarahSu92 <sarah.sundberg@outlook.com>
2025-05-06 14:36:45 +0200

    (feat): added folders and files for users + installed cors to the project

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import 'dotenv/config'
+import express from 'express';
+import cors from 'cors'
+import { connectToDatabase } from './config/db';
+//import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+
+const app = express();
+
+//Middelware
+app.use(express.json()); 
+//app.use(cookieParser()); // This specific middleware parses Cookies
+app.use(cors({
+  origin: "*", // This makes the Express server except request from other domains
+  credentials: true    // Allows cookies sent to this API
+}));   
+
+//Routes
+import usersRouter from './routes/usersRouter';
+//import authRouter from './routes/auth';
+//import { verifyAccessToken } from './middleware/verifyToken';
+app.use('/users', usersRouter);
+//app.use('/auth', authRouter);
+
+//import { verifyAccessToken } from './middleware/verifyToken';
+
+//connect to Mongoose db
+mongoose.connect(process.env.MONGODB_URL || "");
+
+
+// Start server express
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
