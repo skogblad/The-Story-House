5a422850c9607fc9cdc1f0e5447a6b89f91e0eb5
Elin Nilsson <elin@formatfabriken.se>
2025-05-07 23:23:07 +0200

    Add & update: Add book added and changed schema title from books to book.

--- src/controller/booksController.ts
+++ src/controller/booksController.ts
@@ -1,11 +1,12 @@
 import { request, Request, Response } from "express";
-import Books from "../modules/Books";
+import Books from "../modules/Book";
+import Book from "../modules/Book";
 
 // Fetch all books
 export const fetchAllBooks = async (_: Request, res: Response) => {
     try{
         res.json(await Books.find())
-    } catch (error: unknown){
+    } catch (error: unknown) {
         const message = error  instanceof Error ? error.message : 'Unknown error'
         res.status(500).json({error: message})
     }
@@ -16,7 +17,7 @@ export const fetchBook = async (req: Request, res: Response) => {
     try {
         res.json(await Books.findById({_id: req.params.id}));
 
-    } catch (error: unknown){
+    } catch (error: unknown) {
         const message = error  instanceof Error ? error.message : 'Unknown error'
         res.status(500).json({error: message})
     }
@@ -24,10 +25,31 @@ export const fetchBook = async (req: Request, res: Response) => {
 
 // Add book
 export const addBook = async (req: Request, res: Response) => {
-    try {
-        res.json(await Books.insertOne(Books));
+    const {
+        title, 
+        description, 
+        author, 
+        genres,
+        image,
+        published_year
+        //reviews
+        } = req.body
 
-    } catch (error: unknown){
+    try {
+
+        const book = new Book({
+            title: title,
+            description: description,
+            author: author,
+            genres: genres,
+            image: image,
+            published_year: published_year
+            //reviews: reviews
+        });
+        const savedBook = await book.save();
+        res.status(201).json({message: 'Book added.', data: savedBook})
+
+    } catch (error: unknown) {
         const message = error  instanceof Error ? error.message : 'Unknown error'
         res.status(500).json({error: message})
     }
