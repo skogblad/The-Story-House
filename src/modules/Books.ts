3e77894ec357fa766d183b8d41cd49fe27b5b200
Elin Nilsson <elin@formatfabriken.se>
2025-05-07 13:11:30 +0200

    Add: Book schema

---
+++ src/modules/Books.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: String,
+  author: {
+    type: String,
+    required: true
+  },
+  genres: {
+    type: Array,
+    required: true
+  },
+  image: String,
+  published_year: Number,
+  reviews:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Reviews"
+    }]
+});
+
+export default mongoose.model('Book', BookSchema);
