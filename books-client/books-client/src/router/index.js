6b40e50d62380094fb764c52daa97063548d6a1c
Elin Nilsson <elin@formatfabriken.se>
2025-05-13 23:03:09 +0200

    Add: Link to All books

--- books-client/books-client/src/router/index.js
+++ books-client/books-client/src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AllBooksView from '@/views/AllBooksView.vue';
 //import useAuthStore from '@/stores/useAuthStore'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/home', name: 'home', component: HomeView },
+    { path: '/books', name: 'books', component: AllBooksViewView },    
     { path: '/signin', name: 'signin', component: () => import('@/views/SigninView.vue') },
     { path: '/AdminPanel', name: 'admin', component: () => import('@/views/AdminView.vue'), meta: { requiresAuth: true } },
     { path: '/login', name: 'login', component: () => import('@/views/LoginView.vue') },
