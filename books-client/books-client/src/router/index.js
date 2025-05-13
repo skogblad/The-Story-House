989bf0626e1517accc58a846683c6533feb20489
Elin Nilsson <elin@formatfabriken.se>
2025-05-13 22:29:19 +0200

    Add: Routers /books and fetching all books

--- books-client/books-client/src/router/index.js
+++ books-client/books-client/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AllBooksView from '@/views/AllBooksView.vue'
 
 const routes = [
   {
@@ -7,7 +8,12 @@ const routes = [
     name: 'home',
     component: HomeView,
   },
-    {
+  {
+    path: '/books',
+    name: 'books',
+    component: AllBooksView,
+  },  
+  {
       path: '/signin',
       name: 'signin',
       // route level code-splitting
---
+++ books-client/books-client/src/views/AllBooksView.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+import { onMounted } from 'vue';
+
+onMounted(async () => {
+    try {
+        const response = await fetch('http://localhost:3000/books');
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+         console.log(error + 'Byt ut error meddelande');
+
+    }
+});
+
+</script>
+
+<template>
+  <main>
+    <h1>Books</h1>
+  </main>
+</template>
