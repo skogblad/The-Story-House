587cd986ef049899f6da41b4a40411ada7753d04
Elin Nilsson <elin@formatfabriken.se>
2025-05-14 09:02:12 +0200

    Add: Code for showing books and router.

--- books-client/books-client/src/router/index.js
+++ books-client/books-client/src/router/index.js
@@ -7,7 +7,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/home', name: 'home', component: HomeView },
-    { path: '/books', name: 'books', component: AllBooksViewView },    
+    { path: '/books', name: 'books', component: AllBooksView },    
     { path: '/signin', name: 'signin', component: () => import('@/views/SigninView.vue') },
     { path: '/AdminPanel', name: 'admin', component: () => import('@/views/AdminView.vue'), meta: { requiresAuth: true } },
     { path: '/login', name: 'login', component: () => import('@/views/LoginView.vue') },
